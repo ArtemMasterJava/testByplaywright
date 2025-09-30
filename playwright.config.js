73d6b3745f6e3644a1be3088bd1c7ab6913efb43
aanoh <aanohin1@gmail.com>
2025-09-30 21:38:05 +0300

    build: clean history and ignore local chrome binaries

---
+++ playwright.config.js
@@ -0,0 +1,17 @@
+﻿const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  testMatch: /.*\.(spec|test)\.(js|ts)/,
+  fullyParallel: true,
+  forbidOnly: false,
+  retries: 0,
+  reporter: [
+    ['line'],
+    ['html', { open: 'always' }],
+  ],
+  use: { trace: 'on-first-retry', screenshot: 'only-on-failure' },
+  projects: [
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+  ],
+});
